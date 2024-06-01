bdc4f359d83f01b727d5a56027ca6adf14b00719
Refa Andhika <refandika@gmail.com>
2024-06-01 20:28:25 +0700

    Deploy contract to Sepolia with verification then run contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,26 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config()
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const ETHERSCAN_URL = process.env.ETHERSCAN_URL
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 11155111
+    }
+  },
   solidity: "0.8.24",
-};
+  etherscan: {
+    url: ETHERSCAN_URL,
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY
+    }
+  }
+}
